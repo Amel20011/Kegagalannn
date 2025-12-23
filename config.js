1fd46eb45b6c67f790a034a84ca7135551fa5ce1
Amel20011 <amelcns8@gmail.com>
2025-12-23 20:19:34 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Ganti dengan nomor owner (format tanpa @)
+    ownerNumber: ["6281234567890"], 
+    
+    // Nama Bot
+    botName: "💗 WHATSAPP BOT 💗",
+    
+    // Pesan Aesthetic saat di-klik owner
+    ownerContact: "Hubungi owner untuk sewa bot atau lapor bug 🌷",
+    
+    // Link script (opsional)
+    sourceCode: "https://github.com/username/repo"
+};
